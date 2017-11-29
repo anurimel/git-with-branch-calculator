67eaf304a5692eb15f8bd2e2b8cec809be12a60b
Miruna Landstrom <mirunalandstrom@hotmail.com>
2017-11-29 10:41:42 +0100

    Finished the method add and created test for it

--- src/main/java/com/example/maven/git_with_branch_calculator/Calculator.java
+++ src/main/java/com/example/maven/git_with_branch_calculator/Calculator.java
@@ -1,5 +1,14 @@
 package com.example.maven.git_with_branch_calculator;
 
 public class Calculator {
+	public int add (int firstNumber, int secondNumber) {
+		return firstNumber + secondNumber;
+		
+	
+	}
+	public int subtract(int firstNumber, int secondNumber) {
+		return firstNumber - secondNumber;
+				
+	}
 
 }
--- src/test/java/com/example/maven/git_with_branch_calculator/CalculatorTest.java
+++ src/test/java/com/example/maven/git_with_branch_calculator/CalculatorTest.java
@@ -2,13 +2,50 @@ package com.example.maven.git_with_branch_calculator;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+import java.util.logging.Logger;
+
 import org.junit.Test;
 
 public class CalculatorTest {
+	Calculator c = new Calculator();
+	private static final Logger LOG = Logger.getLogger(Calculator.class.getName());
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+		}
+		firstNumber = random.nextInt(100);
+		secondNumber = random.nextInt(100);
+		result = firstNumber + secondNumber;
+		LOG.info("Testing the method add with " + firstNumber + "and" + secondNumber);
+		assertEquals(c.add(firstNumber, secondNumber), result);
+
 	}
 
+
+
+	@Test
+	public void testSubtractMethod() {
+		Random random = new Random();
+		int firstNumber = 0;
+		int secondNumber = 0;
+		int result = 0;
+
+		for (int i = 0; i < 10; i++) {
+
+		}
+		firstNumber = random.nextInt(100);
+		secondNumber = random.nextInt(100);
+		result = firstNumber - secondNumber;
+		LOG.info("Testing the method subtract with " + firstNumber + "and" + secondNumber);
+		assertEquals(c.subtract(firstNumber, secondNumber), result);
+
+	}
 }
