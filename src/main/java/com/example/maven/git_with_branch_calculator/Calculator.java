f65de3ae5aa20586781d5d674b4dbbfd153246cd
Miruna Landstrom <mirunalandstrom@hotmail.com>
2017-11-29 09:44:46 +0100

    Ijust created the project

---
+++ src/main/java/com/example/maven/git_with_branch_calculator/Calculator.java
@@ -0,0 +1,5 @@
+package com.example.maven.git_with_branch_calculator;
+
+public class Calculator {
+
+}
---
+++ src/test/java/com/example/maven/git_with_branch_calculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.example.maven.git_with_branch_calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
